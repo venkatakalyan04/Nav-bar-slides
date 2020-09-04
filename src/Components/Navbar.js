949635dd1cf5479137810da640ff961bf10a39ba
venkatakalyan04 <venkatakalyan47@gmail.com>
2020-09-04 21:09:54 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+
+function Navbar() {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+  
+    return(
+        <>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars  onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-menu-items' >
+        <li className='navbar-toggle'>
+        <Link to='#' className='menu-bars'>
+        <AiIcons.AiOutlineClose />
+        </Link>
+        </li>
+        {SidebarData.map((item, index) =>{
+            console.log('slidebarData')
+         return(
+             <li key={index} className={item.cName}>
+             <link   to={item.path}>
+               {item.icon}
+               <span>{item.title}</span>
+             </link>
+             </li>
+          ) ;  
+        } )}
+        </ul>
+        </nav>
+        </>
+        );
+    }
+
+  export default Navbar;
