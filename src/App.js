c192e43736c00e9c50a1ebb8cfab5d891d5ab59d
venkatakalyan04 <venkatakalyan47@gmail.com>
2020-09-05 15:35:58 +0530

    full changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
     <>
-    <Router>
-    <Navbar/>
-    <Switch>
-    <Route path='/' />
-    </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import   './Navbar.css';
+import   './Hello.css';
 import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons';
+
 
 function Navbar() {
     const [sidebar, setSidebar] = useState(false);
@@ -11,14 +13,15 @@ function Navbar() {
     const showSidebar = () => setSidebar(!sidebar);
   
     return(
-        <>
+        <> 
+        <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars  onClick={showSidebar} />
           </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className='nav-menu-items' >
+        <ul className='nav-menu-items'  onClick={showSidebar}>
         <li className='navbar-toggle'>
         <Link to='#' className='menu-bars'>
         <AiIcons.AiOutlineClose />
@@ -37,6 +40,7 @@ function Navbar() {
         } )}
         </ul>
         </nav>
+        </IconContext.Provider>
         </>
         );
     }
